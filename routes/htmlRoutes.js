8c712b2b73ce36d46767ac4c420f2db2912a8c53
ShareeO <selena74354@yahoo.com>
2021-06-20 18:44:05 -0500

    routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express.Router();
 const path = require('path');
 
+router.get('/',(req,res) => {
+    const resPath = path.join(__dirname,'..','public','index.html');
+    res.sendFile(resPath);
+});
+
 router.get('/notes',(req,res) => {
     const resPath = path.join(__dirname,'..','public','notes.html');
     res.sendFile(resPath);
